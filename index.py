6c4876f6ada8e3a1ceb3654462dd450dc0a3d722
Bergson Jr <bergsonjr@icloud.com>
2020-11-30 15:49:35 -0300

    init

---
+++ index.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+name = "Heisenberg"
+list = [1, 5, 6, 3, 2, 0]
+
+list.sort()
+
+print(list)
+
+if 2 in list:
+    print('lista tem 2')
+
+for char in name:
+    print(char)
